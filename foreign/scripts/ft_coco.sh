97bfe78f8e33b9e35b059ce831be988a87c7ccc5
vlgd <1833622908@qq.com>
2024-10-22 10:19:33 -0700

    plot base ft

--- foreign/scripts/ft_coco.sh
+++ foreign/scripts/ft_coco.sh
@@ -6,18 +6,6 @@ SAVEDIR=checkpoints/coco/${EXPNAME}
 IMAGENET_PRETRAIN=weights/ImageNetPretrained/MSRA/R-101.pkl                            # <-- change it to you path
 IMAGENET_PRETRAIN_TORCH=weights/ImageNetPretrained/torchvision/resnet101-5d3b4d8f.pth  # <-- change it to you path
 
-
-# CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python3 foreign/train_start.py \
-#         --eval-only \
-#         --num-gpus 7 --config-file configs/coco/coco_pretrain.yaml     \
-#         MODEL.WEIGHTS checkpoints/coco/r101_base/model_final.pth \
-#         OUTPUT_DIR ${SAVEDIR}/r101_base \
-
-
-# python3 foreign/model_surgery.py --dataset coco --method remove                         \
-#     --src-path ${SAVEDIR}/r101_base/model_final.pth                        \
-#     --save-dir ${SAVEDIR}/r101_base
-
 BASE_WEIGHT=checkpoints/coco/base_r101/model_reset_remove.pth
 
 ft_base(){
